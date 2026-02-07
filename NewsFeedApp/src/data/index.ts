13df31d6a8fc340c404fe63870ceaf75649ce937
SallySalem <eng.sallysalem@gmail.com>
2026-02-07 19:21:35 +0100

    Clean up wrong commit files

--- NewsFeedApp/src/data/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './postsRepository';
-export * from '../models/post';
--- NewsFeedApp/src/models/post.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-  id?: string;           // uuid v4
-  title: string;
-  content: string;
-  author?: string;
-  created_at?: number;   // epoch ms
-}
